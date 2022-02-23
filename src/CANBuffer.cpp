a2682fc51be8d41905df1d65784e7a65e5e7250f
S. Jansen <sa.jansen1@gmail.com>
2022-02-23 14:17:11 +0200

    Only add unique messages to buffer

--- src/CANBuffer.cpp
+++ src/CANBuffer.cpp
@@ -9,21 +9,49 @@ void CANBuffer::push(CAN_Message message) {
         return;
     }
 
+    CAN_Message newMessage = CAN_Message(message.timestamp, message.id, message.len, message.rxBuf);
+    buffer[addIndex++] = newMessage;
+
     if (addIndex >= BUFFER_SIZE) {
         addIndex = 0;
     }
-
-    CAN_Message newMessage = CAN_Message(message.timestamp, message.id, message.len, message.rxBuf);
-    buffer[addIndex++] = newMessage;
 }
 
 bool CANBuffer::isUnique(CAN_Message message) {
-    return true;
+    // Get last message ID index
+    int lastMessageIndex = -1;
+    for (i = 0; i < BUFFER_SIZE; i++) {
+        int j = addIndex - 1 - i;
+        while (j < 0) {
+            j += BUFFER_SIZE
+        }
+
+        if (buffer[j].id == message.id) {
+            lastMessageIndex = j;
+            break;
+        }
+    }
+
+    if (lastMessageIndex < 0) {
+        return true;
+    }
+
+    // Compare last message with new message
+    if (message->len != buffer[lastMessageIndex].len) {
+        return true;
+    }
+
+    for (uint8_t i = 0; i < message->len; i++) {
+        if (message->rxBuf[i] != buffer[lastMessageIndex].rxBuf[i]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 uint8_t CANBuffer::unreadCount() {
     if (readIndex > addIndex) {
-        return BUFFER_SIZE - 1 - readIndex + addIndex;
+        return BUFFER_SIZE - readIndex + addIndex;
     }
     return addIndex - readIndex;
 }
